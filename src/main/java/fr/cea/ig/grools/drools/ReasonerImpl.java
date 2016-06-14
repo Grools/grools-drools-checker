c7ce2da23fd8bb03c6515bc9f8f3335245c911ec
bioinfornatics <bioinfornatics@fedoraproject.org>
2016-06-14 11:07:50 +0200

    add query get tops knowledges

--- src/main/java/fr/cea/ig/grools/drools/ReasonerImpl.java
+++ src/main/java/fr/cea/ig/grools/drools/ReasonerImpl.java
@@ -280,6 +280,13 @@ public final class ReasonerImpl implements Reasoner {
     }
 
     @Override
+    public Set<PriorKnowledge> getTopsPriorKnowledges(){
+        final Set<PriorKnowledge> priorKnowledges = new HashSet<>();
+        query( "getTopsPriorKnowledges", "$tops", priorKnowledges);
+        return priorKnowledges;
+    }
+
+    @Override
     public Relation getRelation( final Concept $source, final Concept $target, final RelationType $type ){
         return query("getRelation", "$relation", Relation.class, $source, $target, $type);
     }
